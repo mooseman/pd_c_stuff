0fb251df44e82ea25cdd283e60d24c66bc8c9b36
andy <andy@obsidian.(none)>
2012-09-17 21:27:26 +1200

    More work on toysql lexer.

--- toysql_lexer.c
+++ toysql_lexer.c
@@ -14,21 +14,25 @@
 #include <stdbool.h> 
 
 
-char *toks[] = { "select", "from", "where", "and", "or", 
-  "not", "in", "=", "<", ">", "<=", ">=", "!=", ",", ";" }; 
+char *toks[] = { "select", "from", "where", "and", 
+  "or", "not", "in", "=", "<", ">", "<=", ">=", "!=", 
+  "\"", ",", ";" }; 
 
 /* This enum includes tokens for ints, string literals */ 
 /* and variable (column) names. */ 
 enum Toktype { TOK_SELECT, TOK_FROM, TOK_WHERE, TOK_AND, 
-  TOK_IN, TOK_INT, TOK_STRING, TOK_COLNAME, TOK_EQ, TOK_LT, TOK_GT, 
-  TOK_LE, TOK_GE, TOK_NE, TOK_COMMA, TOK_SEMI };    
+  TOK_IN, TOK_INT, TOK_STRING, TOK_COLNAME, TOK_EQ, TOK_LT, 
+  TOK_GT, TOK_LE, TOK_GE, TOK_NE, TOK_DBLQUOTE, TOK_COMMA, 
+  TOK_SEMI, TOK_EOF };    
    
     
 /* Types. */ 
 /* These are the broad category types of tokens.             */ 
 /* They are used to narrow down the token type, like this -  */ 
 /*   if type == T_KEYWORD                                    */ 
-/* { if tok == "select" ( toktype = TOK_SELECT )             */           
+/* { if tok == "select" ( toktype = TOK_SELECT )             */  
+/* Later on, the T_INT and T_STRING types will be used for   */  
+/* doing functions.                                          */          
 enum Type { T_INT, T_STRING, T_KEYWORD, T_COLNAME, T_SYM }; 
 
 /* Value */ 
@@ -43,12 +47,19 @@ typedef struct Value {
 
 
 /* Lexer */ 
+/* ( Look at including a token-buffer and stream-buffer here. ) */ 
 struct lexer { 
 	    FILE *fname; 
 	    int colnum; 
 	    int linenum; 	   
 	    Value *val;    /* As defined above. */ 
-}  	    
+} ;    	    
+
+
+
+
+
+
 
 
 
@@ -68,7 +79,7 @@ int main()
  
  char *test = "select \"foo\" from test;" ; 
   
- tok(test);     
+ /* tok(test);   */   
                                                
  return 0;
  
